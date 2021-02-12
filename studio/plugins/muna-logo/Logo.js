db58646ab44946c757de26398790692599b40d59
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2021-02-12 14:10:35 +0100

    Add nansen logo

--- studio/plugins/muna-logo/Logo.js
+++ studio/plugins/muna-logo/Logo.js
@@ -1,13 +1,16 @@
 // React is installed in the studio and should be treated as a peer dependency
 import React from 'react'
-import {SeidhrIcon} from '@seidhr/react-muna-logo'
+import nansenLogo from './logo.png'
 import styles from './Logo.css'
 
 // We recommend using SVGs as they have both a small footprint and scale well
 // const Logo = () => 'ᛗᚢᚾᚨ'
 const Logo = () => (
   <div>
-    <SeidhrIcon className={styles.logo} /> ᛗᚢᚾᚨ
+    <img src={nansenLogo} className={styles.logo} /> 
+    <strong>
+      Nansen
+    </strong>
   </div>
 )
 
