e8d89e625022bb2955d5ca860924e1ba8c41368f
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2021-03-26 16:04:55 +0100

    Add illustration to sections

--- web/components/Sections/index.js
+++ web/components/Sections/index.js
@@ -1,6 +1,7 @@
 export {default as BigText} from './BigText'
 export {default as Hero} from './Hero'
 export {default as Iframe} from './Iframe'
+export {default as Illustration} from './Illustration'
 export {default as InstagramPost} from './InstagramPost'
 export {default as PageHeader} from './PageHeader'
 export {default as Quote} from './Quote'
