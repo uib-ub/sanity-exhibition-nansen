c11de1fbe5e7b4aa07467eaf25c6798caf2b07f4
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2021-04-12 14:53:07 +0200

    Fix quote sizing

--- web/components/Sections/Quote.js
+++ web/components/Sections/Quote.js
@@ -4,8 +4,8 @@ import PortableTextBlock from '../PortableTextBlock'
 
 export default function Quote(props) {
   return (
-    <Container maxW="3xl" marginTop="10">
-      <Box maxW="xl">
+    <Container maxW="6xl" my="10">
+      <Box maxW="lg" ml="10">
         <PortableTextBlockQuote fontSize="2xl" blocks={props.content} />
         
         {props.credit && (
