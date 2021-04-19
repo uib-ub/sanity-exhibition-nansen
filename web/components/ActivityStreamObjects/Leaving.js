7b97b588c26e448d571a1c5f6afef2fca8fbed3f
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2021-04-19 15:05:01 +0200

    Fix wording on Leaving object

--- web/components/ActivityStreamObjects/Leaving.js
+++ web/components/ActivityStreamObjects/Leaving.js
@@ -7,7 +7,7 @@ export default function Leaving(props) {
   const {separated, separatedFrom, timespan, tookplaceAt} = props
   return (
     <Box maxW={["lg",null, null, null, "2xl"]}>
-      <Timespan display="inline-block" fontWeight="bolder" timespan={timespan} /> – <Link href={`id/${separated._id}`}>{separated.label}</Link> blir meldt ut av {separatedFrom.label}.
+      <Timespan display="inline-block" fontWeight="bolder" timespan={timespan} /> – <Link href={`id/${separated._id}`}>{separated.label}</Link> forlater {separatedFrom.label}.
     </Box>
   )
 }
