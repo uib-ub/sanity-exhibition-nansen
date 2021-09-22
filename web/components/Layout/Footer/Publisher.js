0e70cfe65709a4d972a4693862ab67b40b8356c1
tarjelavik <Tarje.Lavik@ub.uib.no>
2021-09-22 16:37:00 +0200

    Migrate footer to next/image

--- web/components/Layout/Footer/Publisher.js
+++ web/components/Layout/Footer/Publisher.js
@@ -1,5 +1,7 @@
-import { Box, Image, Flex } from '@chakra-ui/react'
-import { imageBuilder } from '../../../lib/sanity'
+import Image from 'next/image'
+import { Box, Flex } from '@chakra-ui/react'
+import { getNextSanityImage } from '../../../lib/sanity.server'
+
 import Link from '../../Link'
 
 export default function Publisher({ publishers }) {
@@ -11,13 +13,16 @@ export default function Publisher({ publishers }) {
     <Flex maxW="xl" marginBottom={5}>
       {publishers &&
         publishers.map((p) => (
-          <Box key={p._id} m="6">
+          <Box key={p._id} m="6" maxW="150px">
             <Link href={`/id/${p._id}`}>
               <Image
-                boxSize={{ base: '100px', md: '140px' }}
-                fit="contain"
-                src={imageBuilder.image(p.image).height(140).fit('fillmax').url()}
                 alt=""
+                {...getNextSanityImage(p.image)}
+                layout="intrinsic"
+                /* sizes="(max-width: 800px) 100vw, 800px" */
+                objectFit="contain"
+                width={150}
+                height={150}
               />
             </Link>
             {/* <Link href={`/id/${p._id}`}>{p.label.no}</Link> */}
