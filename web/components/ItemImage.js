8553dca45ebb80b22e26a36b6c553cf0b9ca0f9f
Tarje Lavik <tarje.lavik@gmail.com>
2021-09-22 20:44:07 +0200

    Remove unused component

--- web/components/ItemImage.js
+++
@@ -1,6 +0,0 @@
-import { imageBuilder } from '../lib/sanity'
-import { Image } from '@chakra-ui/react'
-
-export default function ItemImage({ label, url }) {
-  return <Image src={imageBuilder.image(url).url()} alt={label || 'No label'} />
-}
