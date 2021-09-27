9578cb5a3d17923d0b1fd479b539e2cd9e0e3b68
tarjelavik <Tarje.Lavik@ub.uib.no>
2021-09-27 14:34:45 +0200

    Fix responsivness for actor list

--- web/components/PT/Inserts/ActorInsert.js
+++ web/components/PT/Inserts/ActorInsert.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { Container, Box, Flex, Heading, Text } from '@chakra-ui/react'
+import { Container, Box, Flex, Heading, HStack, Text } from '@chakra-ui/react'
 import Link from '../../Link'
 import { getNextSanityImage } from '../../../lib/sanity.server'
 
@@ -12,7 +12,7 @@ export default function ActorInsert(props) {
   const { _id, label, shortDescription, memberOf } = props
 
   return (
-    <Container maxW={['xl', null, '2xl', null]} mb="5">
+    <Container maxW={['xl', null, '2xl', null]} mb="5" px="0">
       <Flex>
         {/* {image && (
           <Box mr="4">
@@ -26,30 +26,28 @@ export default function ActorInsert(props) {
             />
           </Box>
         )} */}
-        <Box my="3">
-          <Heading size="md" mb="1">
+        <Box>
+          <Heading fontSize={['2xl', null, '3xl', null]} mb="1">
             <Link href={`/id/${_id}`}>{label.no}</Link>
           </Heading>
           {memberOf &&
             memberOf.map((org) => (
-              <Flex key={org._id} alignItems="center" pb="1">
+              <HStack key={org._id} spacing={4} pb="1" justifyContent="baseline">
                 {org.image && (
-                  <Box mr="2">
-                    <Image
-                      alt={label.no}
-                      {...getNextSanityImage(org.image)}
-                      layout="fixed"
-                      /* sizes="(max-width: 800px) 100vw, 800px" */
-                      objectFit="cover"
-                      width={30}
-                      height={30}
-                    />
-                  </Box>
+                  <Image
+                    alt={label.no}
+                    {...getNextSanityImage(org.image)}
+                    layout="fixed"
+                    /* sizes="(max-width: 800px) 100vw, 800px" */
+                    objectFit="cover"
+                    width={36}
+                    height={36}
+                  />
                 )}
-                <Heading size="sm" display="block">
+                <Heading fontSize={['lg', null, 'xl', null]} color="blackAlpha.800" display="block">
                   {org.label.no}
                 </Heading>
-              </Flex>
+              </HStack>
             ))}
           {shortDescription && <Text fontSize="lg">{shortDescription}</Text>}
         </Box>
