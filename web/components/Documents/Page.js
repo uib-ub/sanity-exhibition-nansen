70501e63874ff072d246661d31ba0f578a2f391d
tarjelavik <Tarje.Lavik@ub.uib.no>
2021-09-28 19:15:29 +0200

    Increase Container width

--- web/components/Documents/Page.js
+++ web/components/Documents/Page.js
@@ -5,8 +5,9 @@ import Sections from '../Sections/Sections'
 /* Used for preview */
 export default function Page(data) {
   return (
-    <Container maxWidth="4xl" centerContent>
+    <Container maxWidth="6xl" centerContent>
       {data.content && <Sections sections={data.content} />}
+
       {/* If LinguisticDocument the content is in the body field */}
       {data.body && <PortableTextBlock blocks={data.body} />}
     </Container>
