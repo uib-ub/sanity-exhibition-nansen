33e6df31df7718e735cc0fe9a69642fb8c14b7b5
tarjelavik <Tarje.Lavik@ub.uib.no>
2021-09-28 17:21:29 +0200

    Remove drafts from id when previewing

--- studio/src/parts/resolveProductionUrl.js
+++ studio/src/parts/resolveProductionUrl.js
@@ -1,10 +1,10 @@
+import { replace } from "lodash"
 import { publicDocumentTypes } from "../publicDocumentTypes"
 
 // Any random string, must match SANITY_PREVIEW_SECRET in the Next.js .env.local file
 const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET
-
-const basePath = `/exhibition/nansen-og-bergenserne`
-const remoteUrl = `https://exhibition-nansen.vercel.app`
+const remoteUrl = process.env.SANITY_STUDIO_PREVIEW_DOMAIN
+const basePath = process.env.SANITY_STUDIO_PREVIEW_URL_BASE_PATH
 const localUrl = `http://localhost:3000`
 
 export default function resolveProductionUrl(doc) {
@@ -19,7 +19,7 @@ export default function resolveProductionUrl(doc) {
 
   previewUrl.pathname = `${basePath}/api/preview`
   previewUrl.searchParams.append(`secret`, previewSecret)
-  previewUrl.searchParams.append(`slug`, doc._type === "Route" ? doc?.slug?.current : `id/${doc._id}` ?? `/`)
+  previewUrl.searchParams.append(`slug`, doc._type === "Route" ? doc?.slug?.current : `id/${replace(doc._id, "drafts.", "")}` ?? `/`)
 
   return previewUrl.toString()
 }
