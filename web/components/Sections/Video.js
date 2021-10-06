338dcc368e07cfd5313101a664b34697e4b28c9d
tarjelavik <Tarje.Lavik@ub.uib.no>
2021-10-06 18:05:57 +0200

    Remove unused component

--- web/components/Sections/Video.js
+++ web/components/Sections/Video.js
@@ -10,11 +10,6 @@ export default function Video(props) {
 
   return (
     <Container maxW="2xl" position="relative" my="10">
-      {/* <ReactPlayer
-          url={props.url}
-          controls="true"
-          width="1000px"
-        /> */}
       <AspectRatio ratio={16 / 9} mb="3">
         {url ? (
           <iframe src={url} allowFullScreen title={title ?? 'Video uten tittel'} />
