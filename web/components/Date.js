54c3c7f74c5a5d4fba34fbe9c0f7c0ce57118bda
tarjelavik <Tarje.Lavik@ub.uib.no>
2021-10-06 20:10:18 +0200

    Add dateFormat as props for easier customisation

--- web/components/Date.js
+++ web/components/Date.js
@@ -1,12 +1,12 @@
 import { parseISO, format } from 'date-fns'
 import nb from 'date-fns/locale/nb'
 
-export default function Date({ children }) {
+export default function Date({ dateFormat = 'do LLL yyyy', children }) {
   if (!children) {
     return null
   }
 
   const date = parseISO(children)
 
-  return <time dateTime={children}>{format(date, 'do LLL yyyy', { locale: nb })}</time>
+  return <time dateTime={children}>{format(date, dateFormat, { locale: nb })}</time>
 }
