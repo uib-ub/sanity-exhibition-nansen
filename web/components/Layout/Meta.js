59aa93d25dd8f8d16a02bcfbc83968c1a9ee09b7
Tarje Lavik <tarje.lavik@gmail.com>
2021-10-07 21:24:57 +0200

    Add UiB favicon

--- web/components/Layout/Meta.js
+++ web/components/Layout/Meta.js
@@ -26,11 +26,11 @@ export default function Meta() {
           href={`${basePath}/favicon/favicon-16x16.png`}
         />
         <link rel="manifest" href={`${basePath}/favicon/site.webmanifest`} />
-        <link rel="mask-icon" href={`${basePath}/favicon/safari-pinned-tab.svg`} color="#000000" />
+        <link rel="mask-icon" href={`${basePath}/favicon/safari-pinned-tab.svg`} color="#61223d" />
         <link rel="shortcut icon" href={`${basePath}/favicon/favicon.ico`} />
-        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="msapplication-config" content={`${basePath}/favicon/browserconfig.xml`} />
-        <meta name="theme-color" content="#000" />
+        <meta name="theme-color" content="#ffffff" />
         {/* <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} /> */}
         <meta
           name="description"
